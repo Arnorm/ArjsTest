cdb80deae66bd8ab1791cc5c966f7fef08103afc
Arnorm <82874573+Arnorm@users.noreply.github.com>
2021-04-20 12:05:50 +0200

    Add files via upload

---
+++ coordinates.js
@@ -0,0 +1,22 @@
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Votre position actuelle est :');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude : ${crd.longitude}`);
+    console.log(`La précision est de ${crd.accuracy} mètres.`);
+  }
+  
+  function error(err) {
+    console.log("Ici on a un pb ...");
+    console.warn(`ERREUR (${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+  
