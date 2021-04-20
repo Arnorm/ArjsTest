15b29e9f6647da7ef4d6b7e3cc2f0a7d68b5142e
NORMAND <arthur.normand@capgemini.com>
2021-04-20 15:11:28 +0200

    try to add localization button

--- coordinates.js
+++ coordinates.js
@@ -1,29 +1,33 @@
-// DOES NOT WORK FOR NOW BUT LETS KEEP IT //
+function geoFindMe() {
 
-/* 
-var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  function success(pos) {
-    var crd = pos.coords;
+  const status = document.querySelector('#status');
+  console.log("In Coordinates js");
   
-    console.log('Votre position actuelle est :');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude : ${crd.longitude}`);
-    console.log(`La précision est de ${crd.accuracy} mètres.`);
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
   }
-  
-  function error(err) {
-    console.log("Ici on a un pb ...");
-    console.warn(`ERREUR (${err.code}): ${err.message}`);
-  }
-  */
 
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  
-  console.log("The coordinates script :");  
-  console.log(lat,long);
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
