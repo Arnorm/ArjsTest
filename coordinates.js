0959943be455f6b7a2106285b870fe2eeaaadb3b
NORMAND <arthur.normand@capgemini.com>
2021-04-21 14:53:11 +0200

    switching to gps coordinates

--- coordinates.js
+++ coordinates.js
@@ -1,19 +1,16 @@
 const log = console.log;
     window.onload = () => {
-      let scene = document.querySelector('a-scene'); /* Apply to whole scene */
-
+      let scene = document.querySelector('a-scene'); 
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
           let gps = document.createAttribute('gps-entity-place'),
             arjs = document.createAttribute('arjs'),
             welcome = document.getElementById('welcome');
-
           arjs.value = 'sourceType: webcam; sourceWidth: 1280; sourceHeight: 960; trackingMethod: best; debugUIEnabled: false;';
           gps.value = `latitude: ${position.coords.latitude - 0.001}; longitude: ${position.coords.longitude + 0.001}`;
           log(gps.value);
-          scene.setAttributeNode(gps); /* Apply to whole scene */
+          scene.setAttributeNode(gps); 
           scene.setAttributeNode(arjs);
         });
       }
-
     };
