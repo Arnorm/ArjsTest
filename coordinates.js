7221e0854ee7d9b789982e4746dcbfd288fd0571
NORMAND <arthur.normand@capgemini.com>
2021-04-22 11:35:17 +0200

    removed gps object insertion

--- coordinates.js
+++ coordinates.js
@@ -3,22 +3,23 @@ const log = console.log;
       let scene = document.querySelector('a-scene'); 
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-          let gps = document.createAttribute('gps-entity-place'),
+          //let gps = document.createAttribute('gps-entity-place'),
             arjs = document.createAttribute('arjs'),
             welcome = document.getElementById('welcome');
           arjs.value = 'sourceType: webcam; sourceWidth: 1280; sourceHeight: 960; trackingMethod: best; debugUIEnabled: false;';
-          gps.value = `latitude: ${position.coords.latitude - 0.001}; longitude: ${position.coords.longitude + 0.001}`;
-          scene.setAttributeNode(gps); 
+          //gps.value = `latitude: ${position.coords.latitude - 0.001}; longitude: ${position.coords.longitude + 0.001}`;
+          //scene.setAttributeNode(gps); 
           scene.setAttributeNode(arjs);
           let box = document.createElement('a-box');
-          box.setAttribute('gps-entity-place', 'latitude: ');
+          box.setAttribute('gps-entity-place',
+          `longitude:${position.coords.longitude}; latitude:${position.coords.latitude};`);
           box.setAttribute('scale', '20 20 20');
           box.setAttribute('color', 'red');
           log("Box position :");
           console.log(box.getAttribute('gps-entity-place'));
           scene.appendChild(box);
           log("My position :");
-          log(gps.value);
+          log(position.coords);
         });
       }
     };
