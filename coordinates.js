4d187728bd484a6666e0678e9e73a35844608dcd
NORMAND <arthur.normand@capgemini.com>
2021-04-29 15:00:23 +0200

    Testing suggestion

--- coordinates.js
+++ coordinates.js
@@ -1,26 +1,7 @@
 const log = console.log;
     window.onload = () => {
-      let scene = document.querySelector('a-scene'); 
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-          //let gps = document.createAttribute('gps-entity-place'),
-            arjs = document.createAttribute('arjs'),
-            welcome = document.getElementById('welcome');
-          arjs.value = 'sourceType: webcam; sourceWidth: 1280; sourceHeight: 960; trackingMethod: best; debugUIEnabled: false;';
-          //gps.value = `latitude: ${position.coords.latitude - 0.001}; longitude: ${position.coords.longitude + 0.001}`;
-          //scene.setAttributeNode(gps); 
-          scene.setAttributeNode(arjs);
-          let box = document.createElement('a-box');
-          box.setAttribute('gps-entity-place',
-          `longitude:${position.coords.longitude}; latitude:${position.coords.latitude};`);
-          box.setAttribute('scale', '20 20 20');
-          box.setAttribute('color', 'red');
-          log("Box position :");
-          console.log(box.getAttribute('gps-entity-place'));
-          scene.appendChild(box);
-          log("My position :");
-          log(position.coords);
-
           var displayed_Logs = document.getElementById('logs');
           displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; latitude:${position.coords.latitude}`;
         });
