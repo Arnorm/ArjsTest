9f0e92ea665782406c553109c24d2fa8aba9ecc0
NORMAND <arthur.normand@capgemini.com>
2021-05-03 12:15:28 +0200

    adding onupdate

--- coordinates.js
+++ coordinates.js
@@ -1,5 +1,15 @@
 const log = console.log;
+
     window.onload = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          var displayed_Logs = document.getElementById('logs');
+          displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; latitude:${position.coords.latitude}`;
+        });
+      }
+    };
+
+    window.onchange = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
           var displayed_Logs = document.getElementById('logs');
