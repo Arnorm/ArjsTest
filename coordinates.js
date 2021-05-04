e47e06ebde664fe610ed03bebb6f380d9d074331
NORMAND <arthur.normand@capgemini.com>
2021-05-04 14:12:59 +0200

    logging altitude

--- coordinates.js
+++ coordinates.js
@@ -6,7 +6,10 @@ const log = console.log;
           var displayed_Logs = document.getElementById('logs');
           var ar_Iframe = document.getElementById('arFrame');
           const distance_Message = ar_Iframe.contentWindow.document.getElementById('boxAr').getAttribute('distanceMsg');
-          displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; latitude:${position.coords.latitude} and ${distance_Message}`;
+          displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
+          latitude:${position.coords.latitude};
+          altitude:${position.coords.altitude};
+          and ${distance_Message}`;
         });
       }
     };
