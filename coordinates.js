09c54f32b69bbe44c725eaa0d639261928e82bac
NORMAND <arthur.normand@capgemini.com>
2021-05-04 15:43:26 +0200

    trying with further objects

--- coordinates.js
+++ coordinates.js
@@ -8,8 +8,7 @@ const log = console.log;
           const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
           displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
           latitude:${position.coords.latitude};
-          altitude:${position.coords.altitude};
-          and ${distance_Message}`;
+          and ${distance_Message} away from you`;
         });
       }
     };
