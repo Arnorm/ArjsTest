d0001a5b2f6ac020ac83a599ab829e7d213e23eb
NORMAND <arthur.normand@capgemini.com>
2021-05-04 15:35:40 +0200

    trying to fix logging issues

--- coordinates.js
+++ coordinates.js
@@ -5,11 +5,11 @@ const log = console.log;
         navigator.geolocation.watchPosition(function (position) {
           var displayed_Logs = document.getElementById('logs');
           var ar_Iframe = document.getElementById('arFrame');
-          //const distance_Message = ar_Iframe.contentWindow.document.getElementById('boxAr').getAttribute('distanceMsg');
+          const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
           displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
           latitude:${position.coords.latitude};
           altitude:${position.coords.altitude};
-          and null}`;
+          and ${distance_Message}`;
         });
       }
     };
