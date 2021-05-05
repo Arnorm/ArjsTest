d116befa624af2d8bccb87af9ed5fcc141287419
NORMAND <arthur.normand@capgemini.com>
2021-05-05 17:18:43 +0200

    prefilled values

--- coordinates.js
+++ coordinates.js
@@ -1,26 +1,19 @@
 const log = console.log;
-
-    window.onload = () => {
-      if (navigator.geolocation) {
-
-        
-        navigator.geolocation.watchPosition(function (position) {
-
-          // Managing logging on the left of the screen
-          var displayed_Logs = document.getElementById('logs');
-          var ar_Iframe = document.getElementById('arFrame');
-          const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-          displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
-          latitude:${position.coords.latitude};
-          and ${distance_Message} away from you`;
-
-          // Managing form pre-filling with gps coordinates
-          var lat = document.getElementById('lat');
-          var long = document.getElementById('long');
-          lat.value=position.coords.latitude;
-          long.value=position.coords.longitude;
-        });
-
-        
-      }
-    };
+  window.onload = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(function (position) {
+        // Managing logging on the left of the screen
+        var displayed_Logs = document.getElementById('logs');
+        var ar_Iframe = document.getElementById('arFrame');
+        const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+        displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
+        latitude:${position.coords.latitude};
+        and ${distance_Message} away from you`;
+        // Managing form pre-filling with gps coordinates
+        var lat = document.getElementById('lat');
+        var long = document.getElementById('long');
+        lat.value=position.coords.latitude;
+        long.value=position.coords.longitude;
+      });
+    }
+  };
--- form.js
+++ form.js
@@ -1,7 +1,9 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    console.log("opened form");
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+    console.log("closed form");
   } 
