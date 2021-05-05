3d21348733f821ed8ee96f48ffaecf61b41b609a
NORMAND <arthur.normand@capgemini.com>
2021-05-05 16:27:22 +0200

    prefilled values

--- coordinates.js
+++ coordinates.js
@@ -2,6 +2,8 @@ const log = console.log;
 
     window.onload = () => {
       if (navigator.geolocation) {
+
+        // Managing logging on the left of the screen
         navigator.geolocation.watchPosition(function (position) {
           var displayed_Logs = document.getElementById('logs');
           var ar_Iframe = document.getElementById('arFrame');
@@ -10,5 +12,11 @@ const log = console.log;
           latitude:${position.coords.latitude};
           and ${distance_Message} away from you`;
         });
+
+        // Managing form pre-filling with gps coordinates
+        var lat = document.getElementById('lat');
+        var long = document.getElementById('long');
+        lat.value=position.coords.latitude;
+        long.value=position.coords.longitude;
       }
     };
